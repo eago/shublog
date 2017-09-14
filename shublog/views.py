ceb9046f4740614e520e5f9998b529a1e88e8686
sihai <hhusihai@gmail.com>
2017-09-14 07:58:51 +0200

    view connect sql

--- shublog/views.py
+++ shublog/views.py
@@ -2,8 +2,28 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from models import Article
 
 # Create your views here.
 
+# private methods only used in this file
+
+def get_blog_info(objects):
+    blogs_info=[]
+
+    for blog in objects:
+        blogs_info.append({
+            'title': blog.title,
+            'desc': blog.desc,
+            'pub_time': blog.create_date,
+        })
+    return blogs_info
+
 def index(request):
-    return render(request, "./index.html")
+    blogs = Article.objects.all()
+    blog_info_list = get_blog_info(blogs)
+    content = {
+            'blogs_info': blog_info_list
+            }
+    print blog_info_list
+    return render(request, "./index.html", content)
